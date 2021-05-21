ff9a398f961ede46526897787d8f7f5c23ac5c1b
agnetagsu <agnetagsu@yahoo.es>
2021-05-21 23:34:16 +0200

    Agregar archivos de proyecto.

---
+++ Programa Hola Mundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Programa_Hola_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey(); //Para la ejecución hasta presionar tecla
+        }
+    }
+}
